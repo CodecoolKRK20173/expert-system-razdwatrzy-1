fefde88425ff6a9ef9e23665945bee732a17553e
Roman <adam.romanowski93@gmail.com>
2018-04-30 20:33:16 +0200

    Implement loadXmlDocument() method

--- XMLParser.java
+++ XMLParser.java
@@ -1,3 +1,15 @@
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
 public abstract class XMLParser {
     
+    public void loadXmlDocument(String xmlPath) {
+        
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+
+        File file = new File(xmlPath);
+        Document doc = builder.parse(xmlPath);
+
+     }
 }
