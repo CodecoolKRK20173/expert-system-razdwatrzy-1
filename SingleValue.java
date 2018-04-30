4c8e2c0faf0fa215d236685d83e7e896169a0ee2
Mikolaj <mikolaj.rych@gmail.com>
2018-04-30 20:39:54 +0200

    Define constructor in class SingleValue

--- SingleValue.java
+++ SingleValue.java
@@ -1,4 +1,11 @@
 public class SingleValue {
 
+    private String param;
+    private boolean selectionType;
+
+    public SingleValue(String param, boolean selectionType) {
+        this.param = param;
+        this.selectionType = selectionType;
+    }
     
 }
