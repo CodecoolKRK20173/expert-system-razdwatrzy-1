fc116f8d3a9d0fabace7924257788c9b09a97e9f
Czakero <bartosz.pyrz96@gmail.com>
2018-04-30 21:59:23 +0200

    Implement class field, create class constructor, create empty methods

--- FactRepository.java
+++ FactRepository.java
@@ -1,4 +1,19 @@
-public class FactRepository {
+import java.util.HashMap;
+import java.util.Map;
 
-    
-} 
+public class FactRepository {
+    private Map<integer, Fact> factRepo;
+
+    FactRepository() {
+        this.factRepo = new HashMap<>();
+    }
+
+    public void addFact(Fact fact) {
+
+    }
+
+    public Iterator<Fact> getIterator() {
+
+    }
+
+}
