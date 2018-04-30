247abc58dfae8751bcc6942fa23ca55556f18646
Mikolaj <mikolaj.rych@gmail.com>
2018-04-30 20:41:16 +0200

    Define constructor in class MultipleValue

--- MultipleValue.java
+++ MultipleValue.java
@@ -1,4 +1,13 @@
+import java.util.List;
+
 public class MultipleValue {
 
+    private List<String> params;
+    private boolean selectionType;
+
+    public MultipleValue(List<String> params, boolean selectionType) {
+        this.params = params;
+        this.selectionType = selectionType;
+    }
     
 }
