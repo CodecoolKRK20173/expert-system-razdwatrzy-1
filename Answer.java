6b41a19ffc5e95a724e3e81baa75b8424002b142
Mikolaj <mikolaj.rych@gmail.com>
2018-05-06 20:02:14 +0200

    Implement addValue and evaluateAnswer methods

--- Answer.java
+++ Answer.java
@@ -1,6 +1,19 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.List;
+
 import value.*;
 
 public class Answer {
 
+    private List<Value> values = new ArrayList<>();
+
+    public boolean evaluateAnswerByInput(String input) {
+        return true;
+    }
     
+    public void addValue(Value value) {
+        values.add(value);
+    }
 }
