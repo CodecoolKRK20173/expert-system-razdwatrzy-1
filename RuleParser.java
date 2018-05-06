9eaca815d0af7113b5c0f2b6ca68e3748d0e9976
Roman <adam.romanowski93@gmail.com>
2018-05-06 23:27:37 +0200

    Add fields, implement createQuestion() method

--- RuleParser.java
+++ RuleParser.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.w3c.dom.Document;
@@ -9,49 +10,60 @@ import org.w3c.dom.Text;
 
 public class RuleParser extends XMLParser {
 
+    private Answer answer;
+    private RuleRepository ruleRepository;
+    private String questionId;
+
     public RuleParser(String filePath) {
         super(filePath);
+        this.answer = new Answer();
+        this.ruleRepository = new RuleRepository();
+        this.questionId = "";
     }
 
     public RuleRepository getRuleRepository() {
 
         Document doc = this.getDocument();
-        Element rootRules = doc.getDocumentElement();
-        NodeList rulesNodes = rootRules.getChildNodes();
-
-        RuleRepository ruleRepository = new RuleRepository();
-
-        Answer answer = new Answer();
+        Element root = doc.getDocumentElement();
+        NodeList rulesNodes = root.getChildNodes();
         
         for (int i = 0; i < rulesNodes.getLength(); i++) {
-
             Node ruleNode = rulesNodes.item(i);
             if (ruleNode instanceof Element) {
-                
-                Element rule = (Element) ruleNode;
-                NodeList questions = rule.getChildNodes();
-                String id = rule.getAttribute("id");
-
-                for (int j = 0; j < questions.getLength(); j++) {
-
-                    Node question = questions.item(j);
-
-                    if (question instanceof Element) {
-
-                        Text text = (Text) question.getFirstChild();
-                        String questionString = text.getData().trim();
-
-                        Question q = new Question(id, questionString, answer);
-                        ruleRepository.addQuestion(q);
-                    }
-
-                }
-
+                this.createQuestion(ruleNode);
             }
         }
+        return this.ruleRepository;
+    }
 
-        return ruleRepository;
-    
+    private void createQuestion(Node ruleNode) {
 
+        Element rule = (Element) ruleNode;
+        NodeList questionsList = rule.getChildNodes();
+        this.questionId = rule.getAttribute("id");
+
+        for (int j = 0; j < questionsList.getLength(); j++) {
+
+            Node questionNode = questionsList.item(j);
+            String questionString = "";
+
+            if (questionNode instanceof Element) {
+
+                if (questionNode.getNodeName().equals("Question")) {
+                    Text text = (Text) questionNode.getFirstChild();
+                    questionString = text.getData().trim();
+                } else if (questionNode.getNodeName().equals("Answer")) {
+                    Element elem = (Element) questionNode;
+                    String value = elem.getAttribute("value");
+                    boolean selectionType = true;
+                    this.answer.addValue(new SingleValue(value, selectionType));
+                }
+                
+            }
+
+            Question question = new Question(questionId, questionString, answer);
+            this.ruleRepository.addQuestion(question);
+        }
+        
     }
 }
